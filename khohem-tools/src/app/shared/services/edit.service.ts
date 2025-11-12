03d8b5a922535740092d16f144a62bc9ce8d764c
Shant Derounian <shantderounian@gmail.com>
2025-11-12 23:28:42 +0100

    Removed useless comments

--- khohem-tools/src/app/shared/services/edit.service.ts
+++ khohem-tools/src/app/shared/services/edit.service.ts
@@ -5,15 +5,4 @@ import { Injectable, signal } from "@angular/core";
 })
 export class EditService {
   pictureOffset = signal(0)
-
-  // private edits = signal([]);
-  //
-  // getEdits() {
-  //   return this.edits();
-  // }
-  //
-  // addEdit(edit: any) {
-  //   this.edits.set([...this.edits(), edit]);
-  // }
-
 }
